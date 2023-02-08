6f81145603b2d91a5ff01c78afea954a44e92f90
Dhananjay-Dubey <dhananjaydhanu505@gmail.com>
2023-02-08 07:13:44 +0530

    Method overloading concept

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static class Product
+    {
+        public int product(int x,int y)
+        {
 
+        }
+        public int product(int x, int y , int z)
+        {
+
+        }
+        public double product(double x , double y)
+        {
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(0.1,0.2);
+    }
 }
