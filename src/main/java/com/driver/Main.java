882b9238aa4273de590b08928ec01d06c37014ed
Dhananjay-Dubey <dhananjaydhanu505@gmail.com>
2023-02-08 07:19:04 +0530

    Method overloading changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,15 +5,15 @@ public class Main {
     {
         public int product(int x,int y)
         {
-
+           return 0;
         }
         public int product(int x, int y , int z)
         {
-
+            return 0;
         }
         public double product(double x , double y)
         {
-
+             return 0.0;
         }
 
     }
